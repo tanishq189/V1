f0f239dfb8f7c31a026e0b0c40cc4e493d3b7c79
tanishq vaidya <93328615+tanishq189@users.noreply.github.com>
2023-02-12 16:48:59 +0530

    Add files via upload

---
+++ core/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+
+from item.models import Cities, Item
+
+from .forms import SignupForm
+
+def index(request):
+    items = Item.objects.filter(is_reviewing=False)[0:6]
+    City = Cities.objects.all()
+
+    return render(request, 'core/index.html', {
+        'cities' : City,
+        'items' : items,
+    })
+
+def contact(request):
+    return  render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
